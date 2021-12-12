2c287b71eabdb8f9dbd1b7932361f7f14c555801
ryazanov <ryazanov@gongulus.net>
2021-12-12 14:57:36 +0300

    add tower limit

--- Assets/Scripts/CurrencyManager.cs
+++ Assets/Scripts/CurrencyManager.cs
@@ -18,32 +18,55 @@ namespace TestTD.Systems
     public class CurrencyManager : MonoBehaviour
     {
         [SerializeField, Variable_R] private FloatParameterSO towerCost;
+        [SerializeField, Variable_R] private FloatParameterSO towerLimitRequirement;
         [SerializeField, Variable_R] private IntVariable towerBuildCurrency;
+        [SerializeField, Variable_R] private IntVariable towerMaxLimit;
+        [SerializeField, Variable_R] private IntVariable towerCurrentLimit;
         [SerializeField, Variable_R] private FloatVariable costRefundPercent;
+
+        [SerializeField, Tweakable] private UnityEvent onNotEnoughMoney;
+        [SerializeField, Tweakable] private UnityEvent onNotEnoughLimit;
 
         private float GetTowerCost(TowerData data) => data.GetValue(towerCost);
 
+        public void HandleTowerBuild(TowerDataVariable dataVariable) =>
+            HandleTowerBuild(dataVariable.Value);
+
+        private void HandleTowerBuild(TowerData data)
+        {
+            towerBuildCurrency.DecreaseBy((int) GetTowerCost(data));
+            towerCurrentLimit.DecreaseBy((int) data.GetValue(towerLimitRequirement));
+        }
+        
         public void HandleTowerSold(TowerDataVariable dataVariable) =>
             HandleTowerSold(dataVariable.Value);
         
         private void HandleTowerSold(TowerData data)
         {
-            var costRefund = GetTowerCost(data) * costRefundPercent.Value;
-
-            towerBuildCurrency.IncreaseBy((int) costRefund);
-        }
-
-        public void HandleTowerBuild(TowerDataVariable dataVariable) =>
-            HandleTowerBuild(dataVariable.Value);
-        
-        private void HandleTowerBuild(TowerData data)
-        {
-            towerBuildCurrency.DecreaseBy((int) GetTowerCost(data));
+            towerBuildCurrency.IncreaseBy((int) (GetTowerCost(data) * costRefundPercent.Value));
+            towerCurrentLimit.IncreaseBy((int) data.GetValue(towerLimitRequirement));
         }
 
         public bool CheckCanBuy(TowerData data)
         {
-            return GetTowerCost(data) <= towerBuildCurrency.Value;
+            var haveMoney = GetTowerCost(data) <= towerBuildCurrency.Value;
+
+            if (!haveMoney)
+            {
+                onNotEnoughMoney?.Invoke();
+                return false;
+            }
+
+            var limitRequirement = data.GetValue(towerLimitRequirement);
+            var haveLimit = towerCurrentLimit.Value - limitRequirement >= 0;
+
+            if (!haveLimit)
+            {
+                onNotEnoughLimit?.Invoke();
+                return false;
+            }
+            
+            return true;
         }
     }
 }
