3437f458271bd0ba687600de603c44624fad1a51
ryazanov <ryazanov@gongulus.net>
2021-12-15 20:58:30 +0300

    add parameter validator, add enemy data

---
+++ Assets/Scripts/Tower/TowerGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Sirenix.OdinInspector;
+using DG.Tweening;
+using UniRx;
+using Satisfy.Variables;
+using Satisfy.Attributes;
+
+namespace NMSPC
+{
+    [HideMonoScript]	
+    public class TowerGenerator : MonoBehaviour
+    {
+        void Start()
+        {
+            var update = Observable.EveryUpdate().Where(_ => enabled && gameObject.activeSelf);
+            
+        }
+    }
+}
